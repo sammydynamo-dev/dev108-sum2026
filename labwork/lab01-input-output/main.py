7faee4b95c335763b5ca2656795d64ed0721f2cd
Temitope S. Olugbemi <117564987+sammydynamo-dev@users.noreply.github.com>
2026-07-14 05:15:12 +0000

    Updated labwork/lab01-input-output/main.py with initial lab activity code and user interaction prompts

--- labwork/lab01-input-output/main.py
+++ labwork/lab01-input-output/main.py
@@ -1,10 +1,31 @@
 # DEV 108 Lab Activity 1
-# [Today's Date]
-# [Your Name]
+# 09-07-2026
+# Temitope Olugbemi
 
 # Start writing your code below.
 # Click the "Run" button (or run this file in the terminal with: python main.py)
 # to see your output in the terminal window.
 # Good luck! You can do this - please don't use any AI tools to do this assignment. 
 # That violates the AI Policy for this class and puts your learning at a disadvantage. 
+print("Hello, World!")
+print("10 + 55 =", 10 + 55)
+print("5 x 10 x 2 =", 5 * 10 * 2)
+firstName = "Temitope"
+major = "Software Development - AI"
+print(f"My name is {firstName}.")
+print(f"My major is {major}.")
+num1 = int(input("Enter a number from 0 to 100: "))
+print(f"You entered {num1}")
+num2 = int(input("Enter a number from 10 to 10000: "))
+print(f"You entered {num2}")
+print(f"{num1} x {num2} = {num1 * num2}")
+
+# Experimental
+base = int(input("Enter a base number to square: "))
+print(f"{base} squared is {base ** 2}!")
+age = int(input("How old are you (or turning this year)? "))
+birth_year = 2026 - age
+print(f"Standard math says you were likely born in {birth_year}!")
+hobby = str.lower(input("What is your favorite hobby? "))
+print(f"That's awesome, {firstName}! I hope you get to enjoy {hobby} after finishing this lab.")
 
