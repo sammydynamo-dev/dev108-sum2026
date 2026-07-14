f39d1ed47761404a5fba72bafad9d45cb19290b2
Temitope S. Olugbemi <117564987+sammydynamo-dev@users.noreply.github.com>
2026-07-14 05:32:15 +0000

    (completed): Implement area and perimeter calculation program in labwork/lab02-02-calculator/main.py

--- labwork/lab02-02-calculator/main.py
+++ labwork/lab02-02-calculator/main.py
@@ -1,2 +1,21 @@
-# this is the starting file for Lab Activity 2.2
-# create your own starting code for the instructions provided 
+#!/usr/bin/env python3
+
+# Display a welcome message
+print("The Area and Perimeter program")
+print()
+length = int(input("Please enter the length: "))
+width = int(input("Please enter the width: "))
+print()
+
+# Calculate the area and perimeter
+area = length * width
+perimeter = 2 * (length + width)
+
+# Format and display the result
+print("======================")
+print("Length:\t\t", length)
+print("Width:\t\t", width)
+print("Area:\t\t", area)
+print("Perimeter:\t", perimeter)
+print()
+print("Thanks for using our program. Bye!")
