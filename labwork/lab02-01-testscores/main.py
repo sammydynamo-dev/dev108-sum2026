33ea118bdb856565898684bec392be917121cb42
Temitope S. Olugbemi <117564987+sammydynamo-dev@users.noreply.github.com>
2026-07-14 05:16:58 +0000

    (completed): Refactor labwork-lab02-01-testscores main.py to store individual scores in variables before calculating total and average scores

--- labwork/lab02-01-testscores/main.py
+++ labwork/lab02-01-testscores/main.py
@@ -12,16 +12,20 @@ print("======================")
 
 # get scores from the user
 total_score = 0       # initialize the variable for accumulating scores
-total_score += int(input("Enter test score: "))
-total_score += int(input("Enter test score: "))
-total_score += int(input("Enter test score: "))
+score1 = int(input("Enter test score: "))
+score2 = int(input("Enter test score: "))
+score3 = int(input("Enter test score: "))
+
+# calculate total score
+total_score = score1 + score2 + score3
 
 # calculate average score
 average_score = round(total_score / 3)
              
 # format and display the result
 print("======================")
-print("Total Score:  ", total_score,
-      "\nAverage Score:", average_score)
+print("Your Scores:\t", score1, score2, score3)
+print("Total Score:\t", total_score,
+      "\nAverage Score:\t", average_score)
 print()
 print("Bye")
